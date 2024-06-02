c17b1abd66a58ce52d9b1283f300798cfa237f48
jamesmbcarlson <jamesmbcarlson@gmail.com>
2024-06-02 00:15:07 -0600

    update config with sqlite db

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or ''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
     CACHE_TYPE = 'SimpleCache'
     DEBUG = True
 
 class ProductionConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or ''
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
     CACHE_TYPE = 'SimpleCache'
     DEBUG = False
